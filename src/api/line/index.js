e9124854f9e26e6f2720fc2009fb54fad35e10e2
kkyun8 <kim23302@icloud.com>
2021-02-20 21:09:20 +0900

    line-bot追加

--- src/api/line/index.js
+++ src/api/line/index.js
@@ -1,1 +1,23 @@
-// TODO: line-bot messages
+import line from "@line/bot-sdk";
+
+export const lineConfig = {
+  channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.LINE_CHANNEL_SECRET,
+};
+
+export const lineClient = new line.Client(lineConfig);
+export const lineBot = line;
+
+/**
+ * ラインメッセージリターン
+ * @param {*} event
+ */
+export function lineHandleEvent(event) {
+  if (event.type !== "message" || event.message.type !== "text") {
+    return Promise.resolve(null);
+  }
+  const echo = { type: "text", text: event.message.text };
+
+  // use reply API
+  return lineClient.replyMessage(event.replyToken, echo);
+}
