21183215ab3a29efecd8f285341bebbb700048de
kkyun8 <kim23302@icloud.com>
2021-02-20 21:05:58 +0900

    saveFileをconfigに変更

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,18 @@
+import * as fs from "fs";
 import * as path from "path";
 
 export const __dirname = path.resolve();
 export const tmpOutDir = path.resolve(__dirname, "tmp/out");
 export const modelsDir = path.join(__dirname, "/src/config/faceapi/weights");
+
+export function saveFile(fileName, buf) {
+  if (!fs.existsSync(tmpOutDir)) {
+    fs.mkdirSync(tmpOutDir);
+  }
+
+  fs.writeFileSync(path.resolve(tmpOutDir, fileName), buf);
+}
+
+export const deleteTmpOutDir = () => {
+  fs.rmdirSync(tmpOutDir, { recursive: true });
+};
