bf4edda89b217d24f897c2020326d7a790698536
kkyun8 <kim23302@icloud.com>
2021-04-01 23:43:05 +0900

    square-connectライブラリをsquareに変更

--- src/api/square/index.js
+++ src/api/square/index.js
@@ -1,11 +1,7 @@
-import squareConnect from "square-connect";
+import { Client, Environment } from "square";
 
-export function SquareConnectInit() {
-  const accessToken = process.env.SQUARE_ACCESS_TOKEN;
-  // 初期化
-  const defaultClient = squareConnect.ApiClient.instance;
-  const oauth2 = defaultClient.authentications["oauth2"];
-  oauth2.accessToken = accessToken;
-
-  defaultClient.basePath = process.env.SQUARE_BASEPATH;
-}
+// TODO: environment
+export const squareClient = new Client({
+  environment: Environment.Sandbox,
+  accessToken: process.env.SQUARE_ACCESS_TOKEN,
+});
