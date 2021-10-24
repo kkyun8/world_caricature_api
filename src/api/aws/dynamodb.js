d88ed7a856634e7b525f1b5ae88bebef4868000b
kkyun8 <kim23302@icloud.com>
2021-10-24 23:40:34 +0900

    dynamodb sdk -> apigatewayに変更

--- src/api/aws/dynamodb.js
+++ src/api/aws/dynamodb.js
@@ -1,62 +1,31 @@
-import { ddb } from "../../config/aws/index.js";
-
+import axios from "axios";
+const labelKey = "@LABEL@";
+const orderStatesKey = "@ORDERSTATUS@";
 export let orderStatusLabels = [];
 
 /**
  *
  * @param {*} order
  */
-export async function updateOrder(order) {
-  const params = {
-    ExpressionAttributeNames: {
-      "#OS": "order_status",
-      "#IK": "idempotency_key",
-      "#POI": "payment_order_id",
-      "#PST": "payment_source_type",
-    },
-    ExpressionAttributeValues: {
-      ":os": {
-        S: order.order_status.S,
-      },
-      ":ik": {
-        S: order.idempotency_key.S,
-      },
-      ":poi": {
-        S: order.payment_order_id.S,
-      },
-      ":pst": {
-        S: order.payment_source_type.S,
-      },
-    },
-    Key: {
-      order_id: {
-        S: order.order_id.S,
-      },
-    },
-    TableName: "orders",
-    UpdateExpression: "SET #OS = :os, #IK = :ik, #POI = :poi, #PST = :pst",
-  };
-  const result = ddb.updateItem(params).promise();
+export async function updateOrder(params) {
+  const result = await this.$axios.$post(
+    `/dynamodb/order/status/${orderKey}${orderId}`,
+    params
+  );
   return result;
 }
 /**
  *
  */
-async function scanOrderStatusLabels() {
+async function queryOrderStatusLabels() {
   orderStatusLabels = [];
-  const params = {
-    ExpressionAttributeValues: {
-      ":i": { S: "order_status" },
-    },
-    TableName: "order_item_labels",
-    FilterExpression: "item_id = :i",
-  };
-
-  const result = ddb.scan(params).promise();
-  await result.then((res) => {
-    orderStatusLabels = [...res.Items];
-  });
+  const result = await this.$axios
+    .$get(`/dynamodb/${labelKey}/begins-with/${orderStatesKey}`)
+    .then((res) => {
+      orderStatusLabels = [...res.Items];
+    });
+  return result;
 }
 
 // functions
-scanOrderStatusLabels();
+queryOrderStatusLabels();
